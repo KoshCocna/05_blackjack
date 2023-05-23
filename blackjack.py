792249a66703e5896b16bd1c2ee0697bd806d4db
KoshCocna <76080450+KoshCocna@users.noreply.github.com>
2023-05-24 07:57:44 +1000

    final commit

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,31 @@
+############### Blackjack Project #####################
+
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = {
+##    "A": [1,11],
+##    "2": 2,
+##    "3": 3,
+##    "4": 4,
+##    "5": 5,
+##    "6": 6,
+##    "7": 7,
+##    "8": 8,
+##    "9": 9,
+##    "10": 10,
+##    "J": 10,
+##    "Q": 10,
+##    "K": 10
+##}
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+
 import random
 from blackjackArt import logo
 
